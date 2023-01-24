6f3f51c518a9960e8ed36e7158bbc8789d7da63f
DanielVanNoord <Daniel@optecinc.com>
2023-01-24 12:04:49 -0500

    Updated filter tests

--- fw_test.py
+++ fw_test.py
@@ -3,7 +3,7 @@ import hsfw
 import time
 import copy
 
-
+#This runs the Optec FilterWheel classes through their common methods.
 def run_wheel_tests(wheel):
     print(wheel.serial_number)
     print(wheel.firmware_version)
@@ -18,13 +18,19 @@ def run_wheel_tests(wheel):
 
     wheel.home()
 
+    #Wait for the wheel to finish homing
     while wheel.is_homing:
         time.sleep(.01)
+
+    if not wheel.is_homed:
+        print("Failed to home wheel")
+        return
 
     print(wheel.get_wheel_name('A'))
 
     for i in range(1, wheel.number_of_filters() + 1):
         wheel.move_to_filter(i)
+        #Wait for the wheel to finish moving
         while wheel.is_moving:
             time.sleep(.01)
         print(wheel.get_current_filter())
@@ -49,21 +55,29 @@ def run_wheel_tests(wheel):
 
     wheel.close()
 
+def TestHSFW():
+    for n in hsfw.HSFW.get_serial_numbers():
+        print(n)
 
-for n in hsfw.HSFW.get_serial_numbers():
-    print(n)
+    #Use the first HSFW
+    wheel = hsfw.HSFW(hsfw.HSFW.get_serial_numbers()[0])
+    print(wheel.get_hsfw_status())
+    print(wheel.get_hsfw_description())
 
-wheel = hsfw.HSFW(hsfw.HSFW.get_serial_numbers()[0])
-print(wheel.get_hsfw_status())
-print(wheel.get_hsfw_description())
+    if wheel.error_state != 0:
+        wheel.clear_error()
 
-if wheel.error_state != 0:
-    wheel.clear_error()
+    print("testing HSFW")
+    run_wheel_tests(wheel)  
 
-print("testing HSFW")
-run_wheel_tests(wheel)    
+def TestIFW(comport):
+    print("testing IFW")
+    wheel = ifw.IFW(comport)
+    print(wheel.model)
+    run_wheel_tests(wheel)  
+  
+#raise Exception("Select at least one test and then comment this out")
 
-print("testing IFW")
-ifw = ifw.IFW('COM3')
-print(ifw.model)
-run_wheel_tests(ifw)  
+TestHSFW()
+#Use the COM Port of your IFW / IFW2
+TestIFW("COM3")
